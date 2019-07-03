ce4d66a03019f46b7b5163599354028cf12a7076
cuongld2 <cuongld2@tcbs.com.vn>
2019-07-04 00:29:25 +0700

    First working version for test cloud farm with aws haha

--- src/test/java/zigzagTest.java
+++ src/test/java/zigzagTest.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
@@ -14,56 +15,36 @@ import java.net.URL;
 
 public class zigzagTest {
 
-    public AndroidDriver<MobileElement> driver;
+    public AppiumDriver<MobileElement> driver;
     public WebDriverWait wait;
 
     @BeforeMethod
     public void setup () throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Nexus 5X API 28");
-        caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "9.0");
-        caps.setCapability("skipUnlock","true");
-        caps.setCapability("appPackage", "com.instagram.android");
-        caps.setCapability("appActivity","com.instagram.android.activity.MainTabActivity");
-        caps.setCapability("noReset","false");
-        caps.setCapability("automationName","uiAutomator2");
+//        caps.setCapability("deviceName", "Nexus 5X API 28");
+//        caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
+//        caps.setCapability("platformName", "Android");
+//        caps.setCapability("platformVersion", "9.0");
+//        caps.setCapability("skipUnlock","true");
+//        caps.setCapability("appPackage", "com.socialnmobile.dictapps.notepad.color.note");
+//        caps.setCapability("appActivity","com.socialnmobile.colornote.activity.Main");
+//        caps.setCapability("noReset","false");
+//        caps.setCapability("automationName","uiAutomator2");
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void signUpInstaTest () throws InterruptedException {
+    public void initialTest () throws InterruptedException {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.instagram.android:id/sign_up_with_email_or_phone"))).click();
-
+                (By.id("com.android.packageinstaller:id/permission_allow_button"))).click();
         Assert.assertNotNull(driver.getContext());
-    }
-
-    @Test
-    public void loginInstaTest (){
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.instagram.android:id/log_in_button"))).click();
-
-        Assert.assertNotNull(driver.getContext());
-
-    }
-
-    @Test
-    public void loginInstaWithFbAccountTest(){
-
-
-
     }
 
     @AfterMethod
     public void teardown(){
         driver.quit();
     }
-
-
 
 }
